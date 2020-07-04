005f1a8c74d33d640a8433b56d963594a91750d0
Basil Shubin <basil.shubin@gmail.com>
2020-07-04 11:49:45 +0700

    update example project

--- example/demoapp/models.py
+++ example/demoapp/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 from unitology.models import UnitsFieldMixin
 from unitology.fields import WeightField, HeightField
 
 
-@python_2_unicode_compatible
 class Person(UnitsFieldMixin):
 
     name = models.CharField(max_length=128)
--- example/urls.py
+++ example/urls.py
@@ -1,13 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, re_path
 from django.contrib import admin
-
-from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    re_path(r"^admin/", admin.site.urls),
 ]
 
 urlpatterns += [
-    url(r'^unitology/', include('unitology.urls')),
+    re_path(r"^unitology/", include("unitology.urls")),
 ]
